21faa28896b214c0f79e161affecacdbac514198
Seancheey <adls371@163.com>
2015-04-27 23:46:40 +0800

    restructure teh code of player

--- src/com/seancheey/data/Player.java
+++ src/com/seancheey/data/Player.java
@@ -1,15 +1,33 @@
 package com.seancheey.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Player implements Serializable {
 	private static final long serialVersionUID = 1L;
 	/** the money that the player owns to buy fish */
 	private int money;
+	/** the pond that player can access */
+	private ArrayList<Pond> ponds;
+	/** price that buying will add */
+	private int addedValue = 10;
 
 	public Player(int money) {
+		this(money, new ArrayList<Pond>());
+	}
+
+	public Player(int money, ArrayList<Pond> ponds) {
 		super();
 		this.money = money;
+		this.ponds = ponds;
+	}
+	
+	public int getAddedValue() {
+		return addedValue;
+	}
+
+	public void setAddedValue(int addedValue) {
+		this.addedValue = addedValue;
 	}
 
 	public void earnMoney(int amount) {
