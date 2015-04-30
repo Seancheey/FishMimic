77dc7de663b8671a4756f0e3a915c96f626a7194
Seancheey <adls371@163.com>
2015-04-30 12:29:37 +0800

    restructure the code of menu

--- src/com/seancheey/gui/Menu.java
+++ src/com/seancheey/gui/Menu.java
@@ -2,8 +2,6 @@ package com.seancheey.gui;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -11,8 +9,8 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,82 +20,63 @@ import com.seancheey.data.Map;
 import com.seancheey.interfaces.GUIContainer;
 import com.seancheey.source.BackgroundPond;
 
-public class Menu extends JPanel implements ActionListener {
-
-	/**
-	 * 
-	 */
+public class Menu extends JPanel {
 	private static final long serialVersionUID = 1L;
 	// Container of all the components
-	private ArrayList<JComponent> components = new ArrayList<JComponent>();
-	// unified font for all components
+	private ArrayList<JButton> components = new ArrayList<JButton>();
+	private JLabel title;
+	/** unified font for all components */
 	private static final Font UNIFIED_FONT = new Font("serif", Font.BOLD, 35);
+	/** the container that has this panel */
 	private GUIContainer guiContainer;
 
 	public Menu(GUIContainer guiContainer) {
 		this.guiContainer = guiContainer;
 		setFocusable(false);
+		title = new JLabel(Main.NAME);
+		title.setFont(UNIFIED_FONT);
+		title.setForeground(Color.MAGENTA);
+		title.setAlignmentX(0.5f);
+		title.setAlignmentY(0.5f);
+		add(title);
 		// add all needed components to the container
-		components.add(new JLabel(Main.NAME));
 		components.add(new JButton("New Game"));
-		components.add(new JButton("Read Progress"));
-		components.add(new JButton("About us"));
-		// unify the font of components and add to this panel
-		for (JComponent c : components) {
-			if (c instanceof JButton) {
-				((JButton) c).setFont(UNIFIED_FONT);
-				((JButton) c).addActionListener(this);
-				((JButton) c).setBackground(Color.WHITE);
-			} else if (c instanceof JLabel) {
-				((JLabel) c).setFont(UNIFIED_FONT);
-				((JLabel) c).setForeground(Color.MAGENTA);
+		components.get(0).addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				toNewGamePanel();
 			}
+		});
+		components.add(new JButton("Read Progress"));
+		components.get(1).addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				toLoadedGamePanel();
+			}
+		});
+		components.add(new JButton("About us"));
+		components.get(2).addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				toAboutUs();
+			}
+		});
+		// unify the font of components and add to this panel
+		for (JButton c : components) {
+			c.setFont(UNIFIED_FONT);
+			c.setBackground(Color.WHITE);
 			c.setFocusable(false);
+			c.setAlignmentX(0.5f);
+			c.setAlignmentY(0.5f);
 			add(c);
 		}
-		// create grid bag layout
-		GridBagLayout layout = new GridBagLayout();
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.NONE;
-		c.gridheight = 1;
-		c.gridwidth = 0;
-		c.weightx = 1;
-		c.weighty = 1;
-		for (JComponent component : components) {
-			layout.setConstraints(component, c);
-		}
-		setLayout(layout);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent a) {
-		if (a.getSource() instanceof JButton) {
-			JButton b = (JButton) a.getSource();
-			switch (b.getText()) {
-			case "New Game":
-				guiContainer.switchPanel(this, new GamePanel());
-				break;
-			case "Read Progress":
-				guiContainer.switchPanel(this, new GamePanel(readProgress()));
-				break;
-			case "About us":
-				guiContainer.switchPanel(this, new CreditPanel() {
-					private static final long serialVersionUID = 1L;
-
-					@Override
-					public void backAction() {
-						guiContainer.switchPanel(this, new Menu(guiContainer));
-					}
-				});
-				break;
-			}
-		}
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 	}
 
 	@Override
 	protected void paintComponent(java.awt.Graphics g) {
 		g.drawImage(BackgroundPond.SEA, 0, 0, getWidth(), getHeight(), this);
-	};
+	}
 
 	private Map readProgress() {
 		JFileChooser fileChooser = new JFileChooser("dat");
@@ -125,5 +104,24 @@ public class Menu extends JPanel implements ActionListener {
 		} else {
 			return null;
 		}
+	}
+
+	private void toAboutUs() {
+		guiContainer.switchPanel(this, new CreditPanel() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void backAction() {
+				guiContainer.switchPanel(this, new Menu(guiContainer));
+			}
+		});
+	}
+
+	private void toLoadedGamePanel() {
+		guiContainer.switchPanel(this, new GamePanel(readProgress()));
+	};
+
+	private void toNewGamePanel() {
+		guiContainer.switchPanel(this, new GamePanel());
 	}
 }
