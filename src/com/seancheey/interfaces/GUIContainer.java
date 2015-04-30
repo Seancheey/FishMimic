894b8de514bdf5f316fc0aed38f2e357fb63b807
Seancheey <adls371@163.com>
2015-04-30 13:13:29 +0800

    revies the methods in GUIContainer

--- src/com/seancheey/interfaces/GUIContainer.java
+++ src/com/seancheey/interfaces/GUIContainer.java
@@ -1,7 +1,15 @@
 package com.seancheey.interfaces;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public interface GUIContainer {
 	void switchPanel(JPanel oldPanel, JPanel newPanel);
+
+	Dimension getSize();
+
+	int getWidth();
+
+	int getHeight();
 }
